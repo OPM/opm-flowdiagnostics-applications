e11a3b07adbefacbede1463f6b95c4ba2a4476cb
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2016-08-17 12:33:59 +0200

    Add skeleton utility for computing ToF from ECL output dataset
    This commit adds a (very) simple, incomplete utility
    
        computeToFandTracer
    
    that initialises an ECLGraph instance from an ECLIPSE output set,
    identified by the path-name prefix (directory + filename stem), and
    creates an instance of class Opm::FlowDiagnostics::Toolbox from that
    data.
    
    No actual FD calculations performed at this time.

---
+++ CMakeLists_files.cmake
@@ -0,0 +1,39 @@
+# -*- mode: cmake; tab-width: 2; indent-tabs-mode: t; truncate-lines: t; compile-command: "cmake -Wdev" -*-
+# vim: set filetype=cmake autoindent tabstop=2 shiftwidth=2 noexpandtab softtabstop=2 nowrap:
+
+# This file sets up five lists:
+#       MAIN_SOURCE_FILES     List of compilation units which will be included in
+#                             the library. If it isn't on this list, it won't be
+#                             part of the library. Please try to keep it sorted to
+#                             maintain sanity.
+#
+#       TEST_SOURCE_FILES     List of programs that will be run as unit tests.
+#
+#       TEST_DATA_FILES       Files from the source three that should be made
+#                             available in the corresponding location in the build
+#                             tree in order to run tests there.
+#
+#       EXAMPLE_SOURCE_FILES  Other programs that will be compiled as part of the
+#                             build, but which is not part of the library nor is
+#                             run as tests.
+#
+#       PUBLIC_HEADER_FILES   List of public header files that should be
+#                             distributed together with the library. The source
+#                             files can of course include other files than these;
+#                             you should only add to this list if the *user* of
+#                             the library needs it.
+
+list (APPEND MAIN_SOURCE_FILES
+	opm/utility/ECLGraph.cpp
+	)
+
+list (APPEND TEST_SOURCE_FILES
+	)
+
+list (APPEND EXAMPLE_SOURCE_FILES
+	examples/computeToFandTracers.cpp
+	)
+
+list (APPEND PUBLIC_HEADER_FILES
+	opm/utility/ECLGraph.hpp
+	)
---
+++ opm/utility/ECLGraph.hpp
@@ -0,0 +1,154 @@
+/*
+  Copyright 2016 SINTEF ICT, Applied Mathematics.
+  Copyright 2016 Statoil ASA.
+
+  This file is part of the Open Porous Media Project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef OPM_ECLGRAPH_HEADER_INCLUDED
+#define OPM_ECLGRAPH_HEADER_INCLUDED
+
+#include <opm/core/props/BlackoilPhases.hpp>
+
+#include <cstddef>
+#include <memory>
+#include <vector>
+
+#include <boost/filesystem.hpp>
+
+/// \file
+///
+/// Facility for extracting active cells and neighbourship relations from
+/// on-disk ECLIPSE output, featuring on-demand and cached property loading
+/// from backing object (e.g., restart vectors at various time points).
+
+namespace Opm {
+
+    /// Package an ECLIPSE result set (represented as GRID, INIT, and
+    /// restart files) in form usable with the computational engine
+    /// represented by class \code Opm::FlowDiagnostics::ToolBox \endcode.
+    class ECLGraph
+    {
+    public:
+        using Path = boost::filesystem::path;
+
+        /// Disabled default constructor.
+        ECLGraph() = delete;
+
+        /// Destructor.
+        ~ECLGraph();
+
+        /// Move constructor.
+        ///
+        /// \param[in] rhs Graph from which to appropriate resources.
+        /// Invalid upon return.
+        ECLGraph(ECLGraph&& rhs);
+
+        /// Disabled copy constructor
+        ECLGraph(const ECLGraph& rhs) = delete;
+
+        /// Move assignment operator.
+        ///
+        /// \param[in] rhs Graph from which to appropriate resources.
+        ///
+        /// \return Reference to \code *this \endcode.
+        ECLGraph& operator=(ECLGraph&& rhs);
+
+        /// Disabled assignment operator.
+        ECLGraph& operator=(const ECLGraph& rhs) = delete;
+
+        /// Named constructor.
+        ///
+        /// \param[in] grid Name or prefix of ECL grid (i.e., .GRID or
+        ///                 .EGRID) file.
+        ///
+        /// \param[in] init Name of ECL INIT file corresponding to \p grid
+        ///                 input.  Assumed to provide at least a complete
+        ///                 set of pore-volume values (i.e., for all global
+        ///                 cells defined in the \p grid).
+        ///
+        ///                 If available in the INIT file, the constructor
+        ///                 will also leverage the transmissibility data
+        ///                 when constructing the active cell neighbourship
+        ///                 table.
+        ///
+        /// \return Fully formed ECLIPSE connection graph with property
+        /// associations.
+        static ECLGraph
+        load(const Path& grid, const Path& init);
+
+        /// Assign source object for phase flux calculation.
+        ///
+        /// \param[in] src Name of ECL restart file, possibly unified, from
+        ///                which next set of phase fluxes should be retrieved.
+        void assignFluxDataSource(const Path& src);
+
+        /// Retrieve number of active cells in graph.
+        std::size_t numCells() const;
+
+        /// Retrive number of connections in graph.
+        std::size_t numConnections() const;
+
+        /// Retrive neighbourship relations between active cells.
+        ///
+        /// The \c i-th connection is between active cells \code
+        /// neighbours()[2*i + 0] \endcode and \code neighbours()[2*i + 1]
+        /// \endcode.
+        const std::vector<int>& neighbours() const;
+
+        /// Retrive static pore-volume values on active cells only.
+        ///
+        /// Corresponds to the \c PORV vector in the INIT file, possibly
+        /// restricted to those active cells for which the pore-volume is
+        /// strictly positive.
+        const std::vector<double>& poreVolume() const;
+
+        /// Retrive phase flux on all connections defined by \code
+        /// neighbours() \endcode.
+        ///
+        /// Non-"const" because this potentially loads new data from the
+        /// backing store into internal cache data structures.
+        ///
+        /// \param[in] phase Canonical phase for which to retrive flux.
+        ///
+        /// \param[in] occurrence Selected temporal vector.  Essentially the
+        ///                       report step number.
+        ///
+        /// \return Flux values corresponding to selected phase and report
+        /// step.  Empty if unavailable in the result set (e.g., by querying
+        /// the gas flux in an oil/water system or if the specified \p
+        /// occurrence is not reported due to report frequencies or no flux
+        /// values are output at all).
+        std::vector<double>
+        flux(const BlackoilPhases::PhaseIndex phase,
+             const int                        occurrence = 0);
+
+    private:
+        /// Implementation class.
+        class Impl;
+
+        using ImplPtr = std::unique_ptr<Impl>;
+
+        /// Constructor.  Intentially not \c explicit.
+        ECLGraph(ImplPtr pImpl);
+
+        /// Pointer to implementation.
+        ImplPtr pImpl_;
+    };
+
+} // namespace Opm
+
+#endif // OPM_ECLGRAPH_HEADER_INCLUDED
